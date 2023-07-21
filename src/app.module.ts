9bbf84de2f08dcb35d4ab813f226ff5a46abac52
Pedro Brun <pedroluccavb@gmail.com>
2023-07-21 17:36:42 -0300

    feat(db): add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConversationsModule } from './conversations/conversations.module';
 import { DataSeedsModule } from './data-seeds/data-seeds.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConversationsModule, DataSeedsModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    ConfigModule.forRoot(),
+    ConversationsModule,
+    DataSeedsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
