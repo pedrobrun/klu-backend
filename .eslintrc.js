992a1abb29e78561b84c931664e043c6295a2d1e
Pedro Brun <pedroluccavb@gmail.com>
2023-07-23 00:42:04 -0300

    feat(eslint): ignore warnings on variables that start with _

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,4 +22,12 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
+  '@typescript-eslint/no-unused-vars': [
+    'warn',
+    {
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_',
+      caughtErrorsIgnorePattern: '^_',
+    },
+  ],
 };
